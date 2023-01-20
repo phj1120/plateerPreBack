2dee9dfaafab4eba08a066706c115df198f4d670
PHJ <83655391+phj1120@users.noreply.github.com>
2023-01-20 13:25:10 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-for /f "tokens=5" %p in (' netstat -ano ^| find "LISTENING" ^| find "8080" ') do taskkill /F /PID %
+@@for /f "tokens=5" %p in (' netstat -ano ^| find "LISTENING" ^| find "8080" ') do taskkill /F /PID %
 
 git pull
 
