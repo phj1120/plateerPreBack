c06178192dff784c46a27c144ba7a15f42a1e379
PHJ <83655391+phj1120@users.noreply.github.com>
2023-01-20 13:21:15 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
-for /f "tokens=5" %p in (' netstat -ano ^| find "LISTENING" ^| find "8080" ') do taskkill /F /PID %p
+for /f "tokens=5" %p in (' netstat -ano ^| find "LISTENING" ^| find "8080" ') do taskkill /F /PID %
 
 git pull
+
+cd Backend
 
 start gradlew clean bootrun
