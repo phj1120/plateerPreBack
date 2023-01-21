e9e0aaf68bd45aebde3d38223afa9be32a90f00d
armyso-sseung <90191807+armyso-sseung@users.noreply.github.com>
2023-01-21 13:13:22 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,10 +4,10 @@ cd Backend
 
 for /f "tokens=5" %%p in (' netstat -ano ^| find "LISTENING" ^| find "8080" ') do taskkill /F /PID %%p > NUL
 IF NOT ERRORLEVEL 1 (
-	ECHO Running.
+	ECHO NOT START PROJECT.
 	gradlew clean bootRun
 ) ELSE (
-	ECHO kill OK.
+	ECHO PROJECT KILL OK.
 	start gradlew clean bootRun
 )
 
