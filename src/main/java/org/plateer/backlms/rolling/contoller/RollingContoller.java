4d7289b3607a186b9c47e6ce56cd92412bc2ac6f
Seunghyeon Jung <armyso9506@plateer.com>
2023-01-25 11:41:03 +0900

    Search 및 DB 설정 변경

--- src/main/java/org/plateer/backlms/rolling/contoller/RollingContoller.java
+++ src/main/java/org/plateer/backlms/rolling/contoller/RollingContoller.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.plateer.backlms.common.dto.PageReqDTO;
 import org.plateer.backlms.common.dto.PageResultDTO;
 import org.plateer.backlms.rolling.dto.RollingDTO;
+import org.plateer.backlms.rolling.dto.RollingSearchDTO;
 import org.plateer.backlms.rolling.service.RollingService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,14 +21,31 @@ import java.util.List;
 @Log4j2
 public class RollingContoller {
     private final RollingService rollingService;
-    
+
+
+    /*
+        2022.01.24 정승현 / 페이징 없이 모든 롤링페이퍼 호출 ( Home 메뉴에 사용 )
+     */
     @GetMapping("getRollingAllList")
     public List<RollingDTO> getRollingAllList() {
         return rollingService.getRollingAllList();
     }
 
+
+    /*
+        2022.01.24 정승현 / 페이징으로 롤링페이퍼 호출 ( Rollingpaper List 메뉴에 사용 )
+     */
     @GetMapping("getRollingList")
     public PageResultDTO<RollingDTO> getRollingList(PageReqDTO pageReqDTO) {
         return rollingService.getRollingList(pageReqDTO);
+    }
+
+
+    /*
+        2022.01.25 정승현 / 검색으로 롤링페이퍼 호출 ( Rollingpaper List 메뉴에서 사용 )
+     */
+    @GetMapping("getSearchRollingList")
+    public PageResultDTO<RollingDTO> getSearchRollingList(PageReqDTO pageReqDTO, RollingSearchDTO rollingSearchDTO) {
+        return rollingService.getSearchRollingList(pageReqDTO, rollingSearchDTO);
     }
 }
